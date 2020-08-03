ae2f1a2b43f198bc6db12b2720c229711c1dd4bf
Piotr Kopalko <copalco@gmail.com>
2020-08-03 22:31:10 +0200

    Any year divisible by four and by a hundred is not leap

--- src/main/kotlin/LeapYear.kt
+++ src/main/kotlin/LeapYear.kt
@@ -1,5 +1,8 @@
 class LeapYear {
     fun check(year: Int): Boolean {
+        if (year % 4 == 0 && year % 100 == 0) {
+            return false
+        }
         if (year % 4 == 0) {
             return true
         }
--- src/test/kotlin/LeapYearTest.kt
+++ src/test/kotlin/LeapYearTest.kt
@@ -19,4 +19,9 @@ class LeapYearTest {
         assertThat(leapYear.check(24), equalTo(true))
         assertThat(leapYear.check(2004), equalTo(true))
     }
+
+    @Test fun `Years divisible by four and by hundred are not leap`() {
+        assertThat(leapYear.check(100), equalTo(false))
+        assertThat(leapYear.check(1700), equalTo(false))
+    }
 }
